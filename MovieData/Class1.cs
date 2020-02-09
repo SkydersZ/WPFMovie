b471684cabafac2b0ec4a2d875e9fefec5110652
sofia <sofiane.boucard@live.com>
2020-02-10 00:15:16 +0100

    fix(deleting) : Suppression des éléments inutiles du projet

--- MovieData/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MovieData
-{
-    public class Class1
-    {
-    }
-}
--- MovieManager/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MovieManager
-{
-    public class Class1
-    {
-    }
-}
